b45420b183ff691b330731254ae66539ff6101eb
ebisLab <ebaiden@techie.com>
2022-02-20 15:54:53 -0500

    title linked to home

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -6,7 +6,9 @@ export default function Menu() {
   return (
   <div style={{backgroundColor: 'white', display:"inline-flex", width: '100%'}}>
       <ul style={{width: '100%', fontFamily: 'Julius Sans One, sans-serif', fontWeight: 'bolder'}}>
-        <li style={{display: 'inline', fontFamily: 'Lato, sans-serif', fontSize: '30px'}}>{title}</li>
+        <li style={{display: 'inline', fontFamily: 'Lato, sans-serif', fontSize: '30px'}}>
+          <Link to="/" style={{textDecoration: 'none', color: 'black'}} >{title}</Link>
+          </li>
           <li style={{display: 'inline', padding: '0 5%'}}>
             <Link to="/" style={{textDecoration: 'none'}}> Home</Link>
             </li>
