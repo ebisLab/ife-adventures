d3386fcc66af8fad4d10d2c67033def9a5e3329d
ebisLab <ebaiden@techie.com>
2022-04-02 19:31:12 -0400

    temporary navigation links

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -9,14 +9,29 @@ export default function Menu() {
         <li style={{display: 'inline', fontFamily: 'Lato, sans-serif', fontSize: '30px'}}>
           <Link to="/" style={{textDecoration: 'none', color: 'black'}} >{title}</Link>
           </li>
-          <li style={{display: 'inline', padding: '0 5%'}}>
-            <Link to="/about" style={{textDecoration: 'none'}}> About the author</Link>
+          <li style={{display: 'inline', padding: '0 2%'}}>
+            <Link to="/about" style={{textDecoration: 'none'}}> About the Author</Link>
             </li>
-          {/* <li style={{display: 'inline', padding: '0 5%'}}><a style={{textDecoration: 'none'}} href="">Blog</a></li> */}
-          {/* <li style={{display: 'inline', padding: '0 5%'}}>
-          <Link to="/shop" style={{textDecoration: 'none'}}> Shop </Link>
+            <li style={{display: 'inline', padding: '0 2%'}}>
+            <Link to="/about" style={{textDecoration: 'none'}}> Series</Link>
+            </li>
+            <li style={{display: 'inline', padding: '0 2%'}}>
+            <Link to="/about" style={{textDecoration: 'none'}}> Biblical Inspiration</Link>
+            </li>
+            <li style={{display: 'inline', padding: '0 2%'}}>
+            <Link to="/about" style={{textDecoration: 'none'}}> Ife Journal Entries</Link>
+            </li>
+            <li style={{display: 'inline', padding: '0 2%'}}>
+            <Link to="/about" style={{textDecoration: 'none'}}> Products</Link>
+            </li>
+            {/* <li style={{display: 'inline', padding: '0 2%'}}>
+            <Link to="/about" style={{textDecoration: 'none'}}> DrTeeTee on Youtube</Link>
             </li> */}
-          {/* <li style={{display: 'inline', padding: '0 5%'}}>
+          {/* <li style={{display: 'inline', padding: '0 2%'}}><a style={{textDecoration: 'none'}} href="">Blog</a></li>
+          <li style={{display: 'inline', padding: '0 2%'}}>
+          <Link to="/shop" style={{textDecoration: 'none'}}> Shop </Link>
+            </li>
+          <li style={{display: 'inline', padding: '0 2%'}}>
             <a style={{textDecoration: 'none'}} href="">Contact Us</a></li> */}
       </ul>
   </div>
