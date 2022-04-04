e969ea9b2f9e93454437ccd1ed6cd04a054b0737
ebisLab <ebaiden@techie.com>
2022-04-04 14:08:29 -0400

    active link color

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {title} from '../utils';
 import menu from './menu.css'
-import {Link } from 'react-router-dom';
+import {Link, NavLink } from 'react-router-dom';
 
 export default function Menu() {
   const menu = [
@@ -23,12 +23,13 @@ export default function Menu() {
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" >
         <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
           </svg>
-        </a>):(      <Link to={item.url} class="menu__link">
+        </a>):(      
+        <NavLink to={item.url} className={({isActive})=> `${isActive ? 'menu__link--active' : ''} menu__link`}>
         {item.name}
         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" >
         <path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/>
           </svg>
-        </Link>))}
+        </NavLink>))}
       {/* {menu.map(item=>(
       <Link to={item.url} class="menu__link">
         {item.name}
